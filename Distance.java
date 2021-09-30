7e0f3845c56593a8540ae62b96358cb67d37e36d
prasadv1998 <prasadan1720@gmail.com>
2021-09-30 18:47:18 +0530

    Added: functionality to find the distance of two points

---
+++ Distance.java
@@ -0,0 +1,15 @@
+package javaprograms;
+import java.util.Scanner;
+
+public class Distance {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter x value");
+		int x = sc.nextInt();
+		System.out.println("Enter y value ");
+		int y = sc.nextInt();
+		double distance = Math.sqrt(Math.pow(x, 2)+ Math.pow(y, 2));
+		System.out.println("The Euclidean distance from x,y :" + distance);
+		sc.close();
+	}
+}
