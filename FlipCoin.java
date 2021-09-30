c709f57b71856a758335982c031da14fcc27f435
prasadv1998 <prasadan1720@gmail.com>
2021-09-30 18:15:24 +0530

    Added: to check the percentage of head and tail

---
+++ FlipCoin.java
@@ -0,0 +1,26 @@
+package javaprograms;
+
+public class FlipCoin {
+	public static void main(String[] args) {
+		int heads = 0;
+		int tails = 1;
+		int counter = 1;
+		int numberOfFlips = 21;
+		while (counter != numberOfFlips)
+		{
+			counter++;
+			if(Math.random() <0.5 )
+			{
+				heads++;
+			}
+			else
+			{
+				tails++;
+			}
+		}
+		double headsPercentage = (double) heads/numberOfFlips * 100;
+		System.out.println("Percentage of heads:" + headsPercentage);
+		double tailsPercentage = (double) tails/numberOfFlips * 100;
+		System.out.println("Percentage of tails:" + tailsPercentage);
+	}
+}
